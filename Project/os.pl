954cde3f05650f793c5fd96d0de64f0b30061781
Cody <162437900+cody-p2p@users.noreply.github.com>
2024-11-05 13:39:55 -0700

    Create os.pl
    sub command { ... }

---
+++ Project/os.pl
@@ -0,0 +1,16 @@
+# File contains custom rules or tests for the project
+## Tooling System
+sub command_0  { ... } # command 0 propigates DATA
+sub command_1  { ... } # command 1 COMMAND ISSUANCE
+sub command_2  { ... } # command 2 TOOL Alpha(toolchain)
+sub command_3  { ... } # command 3 B ... bravo command
+sub command_4  { ... } # command 4 C ... charlie command
+sub command_5  { ... } # command 5 D ... delta command
+sub command_6  { ... } # command 6 E ... echlon commnand
+sub command_7  { ... } # command 7 MANUAL
+sub command_8  { ... } # command 8 REFERENCES
+sub command_9  { ... } # command 9 DOCUMENTATION
+sub command_10 { ... } # command 10 SOURCE CODE
+# return 0
+## sub command menu online.
+# EOF
