81bf4a95bdd770e176cc712bfe6822aa3d1a517b
Cody <162437900+Coderad32@users.noreply.github.com>
2025-02-12 08:58:45 -0700

    Update Custom_token.pl
    Fix editor copilot DATA from seq logic.

--- Custom_token.pl
+++ Custom_token.pl
@@ -1,100 +1,63 @@
-!
-@
-#
-$
-%
-^
-&
-*
-(
-!)
-!!
-!@
-!#
-!$
-!%
-!^
-!&
-!*
-!(
-@)
-@!
-@@
-@#
-@$
-@%
-@^
-@&
-@*
-@(
-#)
-#!
-#@
-##
-#$
-#%
-#^
-#&
-#*
-#(
-$)
-$!
-$@
-$#
-$$
-$%
-$^
-$&
-$*
-$(
-%)
-%!
-%@
-%#
-%$
-%%
-%^
-%&
-%*
-%(
-^)
-^!
-^@
-^#
-^$
-^%
-^^
-^&
-^*
-^(
-&)
-&!
-&@
-&#
-&$
-&%
-&^
-&&
-&*
-&(
-*)
-*!
-*@
-*#
-*$
-*%
-*^
-*&
-**
-*(
-()
-(!
-(@
-(#
-($
-(%
-(^
-(&
-(*
-(()
-!))
+# Custom_token.pl
+
+# Tokens starting with '!'
+my @tokens_bang = qw(
+    ! @ !# !$ !% !^ !& !* !( !) !! !@
+);
+
+# Tokens starting with '@'
+my @tokens_at = qw(
+    @) @! @@ @# @$ @% @^ @& @* @(
+);
+
+# Tokens starting with '#'
+my @tokens_hash = qw(
+    #) #! #@ ## #$ #% #^ #& #* #(
+);
+
+# Tokens starting with '$'
+my @tokens_dollar = qw(
+    $) $! $@ $# $$ $% $^ $& $* $(
+);
+
+# Tokens starting with '%'
+my @tokens_percent = qw(
+    %) %! %@ %# %$ %% %^ %& %* %(
+);
+
+# Tokens starting with '^'
+my @tokens_caret = qw(
+    ^) ^! ^@ ^# ^$ ^% ^^ ^& ^* ^(
+);
+
+# Tokens starting with '&'
+my @tokens_amp = qw(
+    &) &! &@ & # & $ & % & ^ & & & * &( 
+);
+
+# Tokens starting with '*'
+my @tokens_star = qw(
+    *) *! *@ *# *$ *% *^ *& ** *(
+);
+
+# Tokens starting with '('
+my @tokens_paren = qw(
+    () (! (@ (# ($ (% (^ (& (* (()
+);
+
+# Tokens starting with '!))'
+my @tokens_bang_close = qw(
+    !))
+);
+
+# Combine all tokens into a single array
+my @all_tokens = (
+    @tokens_bang, @tokens_at, @tokens_hash, @tokens_dollar,
+    @tokens_percent, @tokens_caret, @tokens_amp, @tokens_star,
+    @tokens_paren, @tokens_bang_close
+);
+
+# Print the tokens for verification
+foreach my $token (@all_tokens) {
+    print "$token\n";
+}
